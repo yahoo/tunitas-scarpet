062a039cea59f710ea754bb4e079decc62d2a30c
Wendell Baker <wbaker@verizonmedia.com>
2021-07-23 16:34:49 -0700

    Version v0.1.0-0.devel, builds with tunitas-basics >= 2.0 and with with current 1.0-series background
    builds with tunitas-basics >= 2.0 by depending upon namespace ::tunitas::application (in lieu of tunitas::app)
    namespace ::app is for the local application cluster
    add did-zonegen
    move script-based tests to tests/run
    establish the exception hierarchies and the cascading of the namespaces
    modernist stylings of c.main.xcpp and application rigging-level testing of the application options parsing

--- tests/scissor/rigging.sh
+++ tests/scissor/rigging.sh
@@ -1,6 +1,11 @@
 # source ${0%/*}/rigging.sh || exit 70
 # ... just set the path (that is all, for now)
 #
+f=${0##*/}
+b=${f%.test}
+name=$b
+path=/${name?}
+d=$(basename ${0%/*})
+exe=$d # NOT exe=${PWD##*/} because this executes from ${srcdir} not from ${srcdir}/tests/$d
 __topdir=${0%/*}/../..
-export PATH="${__topdir?}/bin:${__topdir?}/sbin:$PATH"
-exe=did-scissor
+export PATH="${__topdir?}/check/bin:${__topdir?}/bin:${__topdir?}/sbin:$PATH"
